e5d6d465ac5bce91db9fcb1bc149aced09b83821
ieatn <ieatnubcake@gmail.com>
2026-05-28 16:20:28 -0700

    Initial Commit

---
+++ testing-xcode/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  testing-xcode
+//
+//  Created by Dennis Zhang on 5/28/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ testing-xcode/testing_xcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  testing_xcodeApp.swift
+//  testing-xcode
+//
+//  Created by Dennis Zhang on 5/28/26.
+//
+
+import SwiftUI
+
+@main
+struct testing_xcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
